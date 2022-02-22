aa5234185ccaad59b3b57ee2558ba5b3524f3ecf
seena18 <seena101.sa@gmail.com>
2022-02-21 20:51:41 -0800

    sub

--- intsub.s
+++ intsub.s
@@ -3,5 +3,24 @@
     .arch armv8-a
     .global intsub
 
-intsub:
+int sub:
+    stp x29,x30,[sp,-32]!
+    stp x19,x20,[sp,16]
+    sxtb x0,w0
+    sxtb x1,w1
+if:
+    cmp x1,0
+    bne else
+    b endif
+else:
+    mov x19,x0
+    EOR x0,x0,x1
+    NOT x19,x19
+    AND x1,x19,x1
+    LSL x1,x1,1
+    bl intadd
+endif:
+    ldp x19,x20,[sp,16]
+    ldp x29,x30,[sp],32
+    ret
 
