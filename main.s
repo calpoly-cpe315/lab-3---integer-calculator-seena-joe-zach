e7967668fe7d1dcd879c9dd4bed41be57545d4b4
Seena18 <seena101.sa@gmail.com>
2022-02-21 19:29:26 -0500

    user input

--- main.s
+++ main.s
@@ -6,8 +6,15 @@
     // --------------------------------
     .global main
 main:
-    // driver function main lives here, modify this for your other functions
-    ldr    w0, printdata
+    // driver function main lives here, modify this for your other functions0
+    stp    x29,x30, [sp,-32]!
+    mov    x29,sp
+    ldr    w0, printprompt1
+    bl     printf
+    ldr	   w0, printprompt1+4
+    ldr    x1, [x29,#28]
+    bl     scanf
+    ldr    w0, printprompt1+8
     bl     printf
 
 
@@ -24,16 +31,34 @@ loop: ldr     w0, =scanchar
       cmp     w0, w1          // Compare user's answer to char 'y'
       b       loop            // branch to appropriate location
 
+printprompt1:
+    .word numprompt1
+    .word num1
+    .word numprompt2
+    .word num2
+    .word opprompt
+    .word op
+    .word result
+    .word again
 yes:
     .byte   'y'
 scanchar:
     .asciz  " %c"
 numprompt1:
     .asciz  "Enter Number 1: "
+num1:
+    .asciz "%d"
+    .space 1
 numprompt2:
     .asciz  "Enter Number 2: "
+num2:
+    .asciz "%d"
+    .space 1
 opprompt:
     .asciz  "Enter Operation: "
+op:
+    .asciz "%d"
+    .space 1
 result:
     .asciz  "Result: "
 again:
