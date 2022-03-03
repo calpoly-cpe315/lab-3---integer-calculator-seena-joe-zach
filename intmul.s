f4787978a7b839158f17cff0386b472150c220d2
Seena Abed <66761673+seena18@users.noreply.github.com>
2022-03-02 19:38:54 -0800

    Update intmul.s

--- intmul.s
+++ intmul.s
@@ -1,50 +1,38 @@
-    // intmul function in this file
-
+// Replace this file with your own implementation
     .arch armv8-a
     .global intmul
-
+ 
 intmul:
-
+ 
     stp    x29, x30, [sp,-48]!
     stp    x19, x20, [sp, 16]
     stp    x21, x22, [sp, 32]
     
+    mov    x19, x0  //a
+    mov    x20, x1  //b
+    mov    x22, 0 // count
+    mov    x23, 0 // answer 
 
-    mov x19, x0  //a
-    mov x20, x1  //b
-
-    cmp x19, #0
-    beq zero
-
-    cmp x20, #0
-    beq zero
-    
-    mov x22, 0 //total
 loop:
-    // add a to total
-    mov x0, x22
-    mov x1, x19
-    bl intadd
-    mov x22, x0
-    
-    //subtract 1 from b
-    mov x0, x20
-    mov x1, 1
-    bl intsub
-    mov x20, x0
-
-    cmp x20, 0
-    b.eq finish
-    b loop
-    
-
-zero:
-    mov x0, #0
-    b finish
-
-
+    //  if (m % 2 == 1)                
+    and    x25, x20, 1
+    cmp    x25, 1
+    beq    odd
+even:
+		// count++;
+    //    m /= 2;
+    add x22, x22, 1
+    lsr x20, x20, 1
+	cmp    x20, 0
+    beq finish
+    b  loop
+odd:
+    //         ans += n << count;
+    lsl    x9, x19, x22
+    add    x23, x9, x23
+    b even
 finish:
-    mov x0, x22
+    mov    x0, x23
     ldp    x19, x20, [sp, 16]
     ldp    x21, x22, [sp, 32]
     ldp    x29, x30, [sp], 48    
